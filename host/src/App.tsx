6672161aab4911acafa4c2e24f723928172d9c05
chenweilong <340254797@qq.com>
2024-10-12 16:43:43 +0800

    test webworker

--- host/src/App.tsx
+++ host/src/App.tsx
@@ -1,17 +1,17 @@
 import './App.css';
 import Test from 'federation_provider/button';
-// import { useEffect } from 'react';
 import 'remote-web-worker';
+import { useEffect } from 'react';
 
 const App = () => {
-  // useEffect(() => {
-  //   const worker = new Worker(new URL('./test.worker.ts', import.meta.url));
-  //   worker.onmessage = (e) => {
-  //     console.log(e);
-  //   };
-  //
-  //   worker.postMessage([1, 2, 3]);
-  // }, []);
+  useEffect(() => {
+    const worker = new Worker(new URL('./test.worker.ts', import.meta.url));
+    worker.onmessage = (e) => {
+      console.log(e);
+    };
+
+    worker.postMessage([1, 2, 3]);
+  }, []);
   return (
     <div className="content">
       123123
